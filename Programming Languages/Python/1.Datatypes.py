d9c026efd51369034b12c34c3fe308faf63074a7
Rahul Boyina <37464540+rahulboyina21@users.noreply.github.com>
2025-02-14 19:12:53 -0600

    datatypes in python

---
+++ Programming Languages/Python/1.Datatypes.py
@@ -0,0 +1,31 @@
+# Numbers 
+
+var1=123
+var2=123.456
+var3=1j
+
+print(var1,var2,var3)
+
+# Strings
+
+var4= "A"
+var5 = "I am Rahul"
+
+print(var4,var5)
+
+#boolean
+
+var6 = True
+var7 = False
+
+print(var6, var7)
+
+var8 = [1,2,3]
+
+var9 = {1,2,3} 
+
+var10 = (1,2,3)
+
+var11 = {'q':'qwerty','w':'hellboy'}
+
+print(var8,var9,var10,var11)
