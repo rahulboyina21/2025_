5087a0b7f48d41aba3e8fbb1d202e3b311c6e219
Rahul Boyina <37464540+rahulboyina21@users.noreply.github.com>
2025-02-14 19:17:29 -0600

    conditional statements

---
+++ Programming Languages/Python/2.Control_Flow.py
@@ -0,0 +1,6 @@
+if 2<3:
+    print("yooo 2 is less than 3")
+elif "will never execute"<0:
+    print("You will never see this in output")
+else:
+    print("There is a void in the nature it's not about survial it is about dominance in the presence of comfort nothing can be achived")
