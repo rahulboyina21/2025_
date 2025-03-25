d9c95c4e49acdfaf8f5c0f3a68041e52ceeaae60
Rahul Boyina <37464540+rahulboyina21@users.noreply.github.com>
2025-03-25 18:45:22 -0500

    Time: 63 ms (54.35%) | Memory: 32.8 MB (23.88%) - LeetSync

---
+++ 53-maximum-subarray/maximum-subarray.py
@@ -0,0 +1,28 @@
+# class Solution:
+#     def maxSubArray(self, nums: List[int]) -> int:
+#         mxsum,csum=-float('inf'),0
+#         size=len(nums)
+#         if size==1:
+#             return nums[0]
+#         for i in range(size):
+#             if csum<0:
+#                 csum=0
+#             csum+=nums[i]
+#             mxsum=max(mxsum,csum)
+        
+#         return mxsum
+        
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        mxsum,csum=max(nums),0
+        size=len(nums)
+        if size==1:
+            return nums[0]
+        for i in range(size):
+            if csum<0:
+                csum=0
+            csum+=nums[i]
+            mxsum=max(mxsum,csum)
+        
+        return mxsum
+        
