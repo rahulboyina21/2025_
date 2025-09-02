90df28788d51adf613bf73b44c6233400f39c20a
Rahul Boyina <37464540+rahulboyina21@users.noreply.github.com>
2025-09-02 19:23:45 +0000

    The print statement in the 3 languages java/python/go

---
+++ Language/01.Hello_Word/hello_word.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+// main function
+func main() {
+    fmt.Println("Hello, World!")
+
+    // Example: variables
+    number := 10
+    name := "Rahul"
+    fmt.Printf("Number: %d, Name: %s\n", number, name)
+}
+
---
+++ Language/01.Hello_Word/hello_word.java
@@ -0,0 +1,6 @@
+public class hello_word{
+    public static void main(String[] args) {
+        // Print a message
+        System.out.println("Hello, World!");
+    }
+    }
