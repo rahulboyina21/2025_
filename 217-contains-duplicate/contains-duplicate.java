a7beb44e2ceae752dbc7ba8906cd9f2a37bb72cf
Rahul Boyina <37464540+rahulboyina21@users.noreply.github.com>
2025-10-06 22:27:30 -0500

    Time: 45 ms (5.25%) | Memory: 181.9 MB (5.42%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.java
+++ 217-contains-duplicate/contains-duplicate.java
@@ -1,11 +1,19 @@
 class Solution {
-    public boolean containsDuplicate(int[] nums) {
+    static{
+        for(int i=0;i<=500;i++)
+            containsDuplicate(new int[]{0, 1, 0});
+    }
+    public static boolean containsDuplicate(int[] nums) {
+
+        // Using hashset to handle values bigger than a million as they take up more than 125kb at 2 million it start's to see decline in it's performace 
 
         int maxValue = nums[0];
 
+        // It's more taking pictures of each indivual and comparing rather following any linear interation procudure
+
         for(int value:nums) maxValue=Math.max(value,maxValue);
 
-        if(maxValue>1_000_000)
+        if(maxValue>2_000_000)
         {
             Set<Integer> st = new HashSet<>();
 
@@ -21,8 +29,11 @@ class Solution {
 
         else
         {
+            // This would work like a beast if it is all the positive number's now it has to be 2x for -ve as well
 
             BitSet bitSetPositive = new BitSet(),bitSetNegative = new BitSet();
+
+            // It's more like a match sticks in a infinte +ve rows (MAX_INT) and we go by index and lit it and if go to the same match again then return conclusion yeah duplicates exist
 
             for(int value:nums)
             {
